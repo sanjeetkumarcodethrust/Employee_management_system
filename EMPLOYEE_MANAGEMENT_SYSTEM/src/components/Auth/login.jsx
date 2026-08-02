a006a53bc708919fdd1fd9948c93ae818e04e4bc
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-08-02 20:14:07 +0530

    creatting a login page

--- EMPLOYEE_MANAGEMENT_SYSTEM/src/components/Auth/login.jsx
+++ EMPLOYEE_MANAGEMENT_SYSTEM/src/components/Auth/login.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const login = () => {
   return (
-    <div className='flex item-center justify-center '>
+    <div className='flex h-screen w-screen items-center justify-center '>
        <div className= 'border-2 border-red-600'>
-        <form >
+        <form  className='flex flex-col items-center justify-center gap-4 p-4'>
             <input type="email" placeholder='enter your email'/>
             <input type="password" placeholder='enter your password'/>
+            <button>Log in</button>
         </form>
        </div>
     </div>
