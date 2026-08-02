76f8e6f445a5e36a464ac24ec5dfd04adf66a220
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-08-02 21:00:47 +0530

    generate the login page

--- EMPLOYEE_MANAGEMENT_SYSTEM/src/components/Auth/login.jsx
+++ EMPLOYEE_MANAGEMENT_SYSTEM/src/components/Auth/login.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 const login = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  }
   return (
     <div className='flex h-screen w-screen items-center justify-center '>
        <div className= 'border-2 border-red-600'>
-        <form  className='flex flex-col items-center justify-center gap-4 p-4'>
-            <input type="email" placeholder='enter your email'/>
-            <input type="password" placeholder='enter your password'/>
-            <button>Log in</button>
+        <form  onSubmit={(e)=>{
+          submitHandler(e)
+        }} className='flex flex-col items-center justify-center gap-4 p-4'>
+            <input className='border border-gray-300 p-2 rounded-full' type="email" placeholder='enter your email'/>
+            <input className='border border-gray-300 p-2 rounded-full' type="password" placeholder='enter your password'/>
+            <button className='bg-blue-500 text-white p-2 rounded-full'>Log in</button>
         </form>
        </div>
     </div>
