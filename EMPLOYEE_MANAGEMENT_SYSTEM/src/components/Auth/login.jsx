3f5d232455c6afae920aee532525eface8ac8feb
Sanjeet kumar <sanjeetkum960@gmail.com>
2026-08-03 18:58:55 +0530

    generate the login page

--- EMPLOYEE_MANAGEMENT_SYSTEM/src/components/Auth/login.jsx
+++ EMPLOYEE_MANAGEMENT_SYSTEM/src/components/Auth/login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const login = () => {
+
+const [email, setEmail] = React.useState("");
+const [password, setPassword] = React.useState("");
+
+
+
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("form submitted");
+    console.log("Email:", email);
+    console.log("Password:", password);
+    setEmail("");
+    setPassword("");
   }
   return (
     <div className='flex h-screen w-screen items-center justify-center '>
@@ -11,8 +21,13 @@ const login = () => {
         <form  onSubmit={(e)=>{
           submitHandler(e)
         }} className='flex flex-col items-center justify-center gap-4 p-4'>
-            <input className='border border-gray-300 p-2 rounded-full' type="email" placeholder='enter your email'/>
-            <input className='border border-gray-300 p-2 rounded-full' type="password" placeholder='enter your password'/>
+            <input 
+             value={email} 
+             onChange={(e) => setEmail(e.target.value)}
+             className='border border-gray-300 p-2 rounded-full' type="email" placeholder='enter your email' onChange={(e) => setEmail(e.target.value)}/>
+            <input value={password} 
+            onChange={(e) => setPassword(e.target.value)}
+            className='border border-gray-300 p-2 rounded-full' type="password" placeholder='enter your password' onChange={(e) => setPassword(e.target.value)}/>
             <button className='bg-blue-500 text-white p-2 rounded-full'>Log in</button>
         </form>
        </div>
